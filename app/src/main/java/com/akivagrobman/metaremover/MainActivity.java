bed2795bc028a5cc496ff979c68d08c9ef689197
akiva <akiva.grobman@gmail.com>
2022-04-20 13:15:06 +0300

    Move wrong usage toast to onResume

--- app/src/main/java/com/akivagrobman/metaremover/MainActivity.java
+++ app/src/main/java/com/akivagrobman/metaremover/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
@@ -16,10 +17,13 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Intent receivedIntent = getIntent();
         String receivedType = receivedIntent.getType();
@@ -40,7 +44,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-        } else if (receivedAction.equals(Intent.ACTION_MAIN)) {
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String receivedAction = getIntent().getAction();
+        if (receivedAction.equals(Intent.ACTION_MAIN)) {
             Toast.makeText(this,
                     "This app shouldn't be opened this way",
                     Toast.LENGTH_SHORT).show();
